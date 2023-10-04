dd8e0384016d3d2f214c29d57a5be8551a0404ba
tofickbrodaga <mshdtlv@gmail.com>
2023-10-04 11:31:40 +0300

    Added test for function degree

---
+++ test_ball.py
@@ -0,0 +1,26 @@
+"""A module for testing main degree ball function."""
+import pytest
+
+from main import degree
+
+data = [
+    ((10.5, 13.5, 13.1), 204.12),
+    ((12.1, 4.1, 7), 115.89),
+    ((10, 10, 5, 6), 168.54)
+]
+
+
+@pytest.mark.parametrize('angle, expected', data)
+def test_degree_ball(angle: float, expected: int) -> None:
+    """Test degree function with data.
+
+    Args:
+        radius: float, ball radius.
+        acceleration: float, acceleration of the ball as it rolls.
+        time: float, the time he's been rolling.
+        velocity: float, initial speed.
+
+    Asserts:
+        Float angle of shifting the ball.
+    """
+    assert degree(*angle) == expected
