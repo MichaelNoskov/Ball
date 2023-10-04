787fc78e6e9443cbf71f57b072f704d7f0c6975e
Marko <90ts@mail.ru>
2023-10-04 15:12:01 +0300

    add files

--- test_main.py
+++ test_main.py
@@ -1,25 +1,69 @@
-"""A module for testing main degree ball function."""
+"""Module for testing test function."""
 import pytest
 
 from main import degree
 
-test_data = [
-    ((10.5, 13.5, 13.1), 200.91),
-    ((12.1, 4.1, 7), 115.65),
-    ((10, 10, 5, 6), 168.08),
-    ((0, 0, 0, 0), 0),
-]
+THE_DATA = (
+    (5, 12, 7, 3, (9.63)),
+    (1, 2, 3, 4, (123.21)),
+    (1, 3, 0, 0, (0)),
+    (234, 123, 666, 90, (127.29)),
+)
+
+THE_DATA_FALIURE = (
+    (0, 0, 0, 0, (pytest.raises(ValueError))),
+    (-5, 4, 5, 6, (pytest.raises(ValueError))),
+)
 
 
-@pytest.mark.parametrize('angle, expected', test_data)
-def test_degree_ball(angle: float, expected: float) -> None:
-    """Test degree function with data.
+@pytest.mark.parametrize(
+    'test_radius, test_acceleration, test_time, test_velocity, expected',
+    THE_DATA,
+)
+def test_degree(
+    test_radius: float,
+    test_acceleration: float,
+    test_time: float,
+    test_velocity: float,
+    expected: tuple,
+) -> None:
+    """Test degree function.
 
     Args:
-        angle: float, angle of shifting the ball
-        expected: an actual expected degree result.
+        test_radius: [float] - The radius.
+        test_acceleration: [float] - The acceleration.
+        test_time: [float] - The time.
+        test_velocity: [float] - The start velocity.
+        expected: [tuple[float]] - expected result of function.
 
     Asserts:
-        float angle of shifting the ball.
+        True if test_degree returns expected answer.
     """
-    assert degree(*angle) == expected
+    assert degree(test_radius, test_acceleration, test_time, test_velocity) == expected
+
+
+@pytest.mark.parametrize(
+    'test_radius, test_acceleration, test_time, test_velocity, expected',
+    THE_DATA_FALIURE,
+)
+def test_degree_failure(
+    test_radius: float,
+    test_acceleration: float,
+    test_time: float,
+    test_velocity: float,
+    expected: tuple,
+) -> None:
+    """Test degree function.
+
+    Args:
+        test_radius: [float] - The radius.
+        test_acceleration: [float] - The acceleration.
+        test_time: [float] - The time.
+        test_velocity: [float] - The start velocity.
+        expected: [tuple[float]] - expected result of function.
+
+    Asserts:
+        True if test_degree returns expected answer.
+    """
+    with expected:
+        assert degree(test_radius, test_acceleration, test_time, test_velocity) is not None
