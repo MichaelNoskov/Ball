7e5feb261df3e41ba35ae9b1b91057fd21989209
MichaelNoskov <prosto.ya.chelovekn@mail.ru>
2023-10-04 14:34:41 +0300

    added docstring

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-"""Scrolling Ball"""
+"""Scrolling Ball."""
+from math import pi
 
 
-def degree(radius: float, acceleration: float, time: float, velocity: float = 0.0) -> float:
-    """Calculates the angle of shifting the ball
+def degree(radius: float, acceleration: float, time: float, velocity: float = 0) -> float:
+    """Calculate the angle of shifting the ball.
 
-    :param radius: float
-    :param acceleration: float
-    :param time: float
-    :param velocity: float
-    :return: float
+    Args:
+        radius: float, ball radius.
+        acceleration: float, acceleration of the ball as it rolls.
+        time: float, the time he's been rolling.
+        velocity: float, initial speed.
+
+    Returns:
+        Float angle of shifting the ball.
     """
-    s = velocity * time + acceleration * time ** 2 / 2
-    c = 2 * 3.14 * radius
-    angle = s % c / (c/360)
+    circle = 360
+    road = velocity * time + acceleration * time ** 2 / 2
+    ball_len = 2 * pi * radius
+    angle = road % ball_len / (ball_len/circle)
     return round(angle, 2)
