8f6d225761460c8132acd35f635097b6c7f72151
tofickbrodaga <mshdtlv@gmail.com>
2023-10-04 11:51:41 +0300

    Minor changes in test_ball.py

--- test_ball.py
+++ test_ball.py
@@ -3,15 +3,15 @@ import pytest
 
 from main import degree
 
-data = [
+test_data = [
     ((10.5, 13.5, 13.1), 200.91),
     ((12.1, 4.1, 7), 115.65),
     ((10, 10, 5, 6), 168.08),
-    ((0, 0, 0, 0), 0)
+    ((0, 0, 0, 0), 0),
 ]
 
 
-@pytest.mark.parametrize('angle, expected', data)
+@pytest.mark.parametrize('angle, excepted', test_data)
 def test_degree_ball(angle: float, expected: int) -> None:
     """Test degree function with data.
 
